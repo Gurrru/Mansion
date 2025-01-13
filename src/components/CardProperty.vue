62fd882c6d7ce7e0698068aef167aa6716477f01
khalidhardiansyah <muhammad1900018293@webmail.uad.ac.id>
2025-01-13 22:05:12 +0700

    change prop listview

--- src/components/CardProperty.vue
+++ src/components/CardProperty.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="w-full h-[455px] sm:w-[270px] border rounded-xl overflow-hidden">
+    <div class="w-full h-[435px] sm:min-w-[270px] xl:w-full border rounded-xl overflow-hidden">
         <div class="h-[275px]">
             <img class=" aspect-[17.25/13.875] w-full h-full  object-cover bg-center " src="https://placehold.co/400"
                 alt="" srcset="">
         </div>
-        <div class="p-5">
+        <div class="h-full p-5 bg-white">
             <p class="text-lg font-bold capitalize ">pandak, bantul</p>
             <div class="flex py-1 text-sm border-t border-b border-gray-200 text-opacity-85 gap-x-3">
                 <p>3 Kamar tidur</p>
@@ -14,7 +14,7 @@
             <p class="pb-1 border-b border-gray-200 text-opacity-85 ">
                 Apartemen
             </p>
-            <div class="flex justify-between w-full mt-7 text-opacity-85">
+            <div class="flex justify-between w-full mt-4 text-opacity-85">
                 <p class="text-base ">
                     Rp. <span class="font-semibold ">630.000</span>
                 </p>
--- src/views/Prop-ListView.vue
+++ src/views/Prop-ListView.vue
@@ -1,14 +1,48 @@
 <template>
     <div class="pt-20 min-h-dvh">
         <div class="px-5 md:px-20">
-            prop list
+            <div class="flex flex-col sm:flex-row sm:flex-wrap gap-y-5 sm:gap-5 sm:justify-between">
+                <h1 class="text-5xl text-center capitalize sm:basis-full">iconic for good reason</h1>
+                <div class="flex flex-row justify-between sm:basis-full">
+                    <select id="" name="region">
+                        <option value="" selected disabled>Select Region</option>
+                        <option value="">All</option>
+                        <option value="">Yogyakarta</option>
+                        <option value="">Sleman</option>
+                        <option value="">Bantul</option>
+                        <option value="">Kulon Progo</option>
+                        <option value="">Gunung Kidul</option>
+                    </select>
+                    <select id="" name="price">
+                        <option value="" selected>Recently Listed</option>
+                        <option value="">Price High</option>
+                        <option value="">Price Low</option>
 
+                    </select>
+                </div>
+
+                <div class="grid grid-flow-row basis-full sm:grid-cols-2 gap-7 lg:grid-cols-3 xl:grid-cols-4 xl:gap-9">
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                    <CardProperty />
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-
+import CardProperty from '@/components/CardProperty.vue';
 </script>
 
 <style></style>
